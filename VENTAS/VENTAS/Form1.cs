43965aecb272aa5b0c9d06c0b6b9d0fc39034204
Antonio Villatoro <antoniovillatoro8@gmail.com>
2021-02-02 19:58:22 -0600

    agregando frase motivadora

--- VENTAS/VENTAS/Form1.cs
+++ VENTAS/VENTAS/Form1.cs
@@ -19,7 +19,7 @@ namespace VENTAS
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("HOLA MUNDO");
+            MessageBox.Show("TEN FÉ, TODO ESTARÁ BIEN");
         }
     }
 }
